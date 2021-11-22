c21fb4653135f1ac3e19a7c4ed18741d81aa08bb
Lucas Silva Souza <lucassilvadonato23@gmail.com>
2021-11-22 14:31:27 -0300

    script trabalhando com promise e operador ternario

---
+++ promises.js
@@ -0,0 +1,14 @@
+const random = Math.floor(Math.random() * 10)
+const numberRandom = new Promise((resolve, reject) => {
+    (random % 2) === 0 ? resolve({
+        number: random,
+        stauts: 'par'
+    }) : reject({
+        number: random,
+        stauts: 'impar'
+    })
+})
+
+numberRandom.then((result) => console.log(result))
+.catch((err) => console.error(err))
+.finally(() => console.log('--end--'))
