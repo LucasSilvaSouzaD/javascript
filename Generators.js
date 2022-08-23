6c8db4be99b86e1ee2e8451ca2b4bde7464db65f
Lucas Silva Souza <lucassilvadonato23@gmail.com>
2022-08-23 20:04:04 -0300

    [UPDATE] Generators and Symbol

---
+++ Generators.js
@@ -0,0 +1,14 @@
+
+
+async function *table() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    yield number
+    
+}
+
+
+;(async () => {
+    for await (const tab of table()) {
+        console.log("items", table)
+    }
+})()
