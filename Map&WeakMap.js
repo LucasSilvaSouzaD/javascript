8d22f9afef8e0a17d69ede2c458af7ae5fcae7a1
LucasSilvaSouzaD <lucassilvadonato23@gmail.com>
2022-10-18 12:25:29 -0300

    [Update ] Map

--- Map&WeakMap.js
+++ Map&WeakMap.js
@@ -3,9 +3,15 @@ const assert = require('assert')
 const objectMap = new Map()
 
 // declarar uma chave e valor dentro do Map
+// pode ter qualquer tipo como chave
 objectMap
-    .set("1", {nome: 'lucas'})
+    .set(1, {nome: 'lucas'})
 // Map(1) { '1' => { nome: 'lucas' } }
+
+// declarando chave e valor usando o construtor
+const objectMapConstructor = new Map([
+    [1, {nome: 'lucas'}],
+])
 
 for (const item of objectMap) {
     assert.deepStrictEqual(item[0], '1')
@@ -26,4 +32,5 @@ objectMap.delete('1')
 // limpar o objeto Map
 objectMap.clear()
 
+////////////////////////////////////////// --------WeakMap-------- //////////////////////////////////////////
 
