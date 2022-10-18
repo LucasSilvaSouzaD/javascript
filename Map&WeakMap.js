360e4775f78e1bee71e2cfbf1c7b68592be39fa6
LucasSilvaSouzaD <lucassilvadonato23@gmail.com>
2022-10-18 12:21:55 -0300

    new Map()

---
+++ Map&WeakMap.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+
+const objectMap = new Map()
+
+// declarar uma chave e valor dentro do Map
+objectMap
+    .set("1", {nome: 'lucas'})
+// Map(1) { '1' => { nome: 'lucas' } }
+
+for (const item of objectMap) {
+    assert.deepStrictEqual(item[0], '1')
+    assert.deepStrictEqual(item[1], {nome: 'lucas'})
+}
+
+// pegar todas chaves do objeto
+objectMap.keys()
+// pegar todos valores do objeto
+objectMap.values()
+
+// verificar se existe a propriedade dentro do objeto
+assert.ok(objectMap.has('1'))
+
+// excluir uma propriedade a partir da chave
+objectMap.delete('1')
+
+// limpar o objeto Map
+objectMap.clear()
+
+
